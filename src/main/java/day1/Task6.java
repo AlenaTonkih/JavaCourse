f567d980b8b7bf144e5a6e3155a19a35bd6030e2
Alena Tonkih <alenatonkich17@gmail.com>
2022-08-24 15:32:33 +0300

    add day 1

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+            int k = 3;
+            int i = 0;
+            while(i < 10){
+                i++;
+                System.out.println(i + " * " + k + " " + " = " + i * k);
+            }
+    }
+}
