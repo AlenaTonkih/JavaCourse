e524aa5e759da40cfb6fdfe78980a2835a5275f1
Alena Tonkih <alenatonkich17@gmail.com>
2022-08-29 16:37:21 +0300

    add days 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,39 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setBrand("Audi"); //сеттеры - назначают методы
+        car1.setColor("Green");
+        car1.setYear(2013);
 
+        System.out.println("Вывод значения марки автомобиля: " + car1.getBrand());
+        System.out.println("Вывод значения цвета автомобиля: " + car1.getColor());
+        System.out.println("Вывод значения года выпуска автомобиля: " + car1.getYear());
+        //геттеры методы выводят
+    }
+}
+class Car {
+    private String brand;
+    private String color;
+    private int year;
+
+    public void setBrand(String userBrand){
+        brand = userBrand;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public void setColor(String userColor){
+        color = userColor;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setYear(int userYear){
+        year = userYear;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,30 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike("Audi", "Green", 2015);
+    }
+}
+class Motorbike {
+    private String brand;
+    private String color;
+    private int year;
 
+    public Motorbike(String brand, String color, int year){
+        this.brand = brand;
+        this.color = color;
+        this.year = year;
+
+        System.out.println("Марка мотоцикла: " + brand);
+        System.out.println("Цвет мотоцикла: " + color);
+        System.out.println("Год выпуска мотоцикла: " + year);
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getYear(){
+        return year;
     }
 }
