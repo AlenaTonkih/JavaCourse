9bf3ddfe23b81268c8a994a84f0678bef0c6dfc6
Alena Tonkih <alenatonkich17@gmail.com>
2022-09-01 14:56:01 +0300

    day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,20 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+    String numbers = "";
+        long startTime = System.currentTimeMillis();
+    for(int i = 0; i <= 20000; i++)
+        numbers += i + " ";
 
+        //System.out.println(numbers);
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы: " + (stopTime - startTime));
+
+
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder(numbers);
+        System.out.println(sb.toString());
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы StringBuilder: " + (stopTime2 - startTime2));
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,30 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane1 = new Airplane("Boeing", 2010, 30, 10);
+        System.out.println(airplane1);
 
     }
 }
+class Airplane {
+    private String manufacturer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+
+    }
+
+    public String toString() { //используем, чтобы вывести строку, а не хэшкод
+        return manufacturer + ", " + year + ", " + length + ", " + weight;
+
+    }
+}
+
+
